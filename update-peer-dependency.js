3c0669dd3f26afbd1828a421670c0e6f94414f62
dikac <rigel234@yahoo.com>
2022-07-10 17:04:08 +0700

    build: update monorepo version

--- update-peer-dependency.js
+++ update-peer-dependency.js
@@ -3,9 +3,12 @@ import Dirs from '@alirya/monorepo/dir/array/dirs';
 import Files from '@alirya/monorepo/file/array/files';
 import Write from '@alirya/monorepo/json/write';
 import JsonRead from '@alirya/monorepo/json-read';
+import PackageJson from '@alirya/monorepo/dir/package-json';
+
+const dir = PackageJson(__dirname);
 
 Dirs(__dirname, JsonRead(__dirname + '/lerna.json').packages)
-  .then(dirs=>UpdatePeerDependencies(dirs, (dev, pkg, dependency, from, to) => {
+  .then(dirs=>UpdatePeerDependencies([dir, ...dirs], (dev, pkg, dependency, from, to) => {
     const development = dev ? '[dev]' : '';
     console.log(`${pkg}: ${dependency} ${from} -> ${to} ${development}`)
   }))
