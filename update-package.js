18964f4ba74b908ea679d59808cd82a7ea197465
dikac <rigel234@yahoo.com>
2023-12-27 17:10:20 +0700

    ref: update main script

--- update-package.js
+++ update-package.js
@@ -1,11 +1,15 @@
-import Dirs from '@axiona/monorepo/dir/array/dirs';
-import UpdateGlobalDependencies from '@axiona/monorepo/update-global-dependencies';
-import Files from '@axiona/monorepo/json/array/files';
-import JsonRead from '@axiona/monorepo/json-read';
-import MergeRight from '@axiona/monorepo/merge-right';
-import MergeLeft from '@axiona/monorepo/merge-left';
-import Write from '@axiona/monorepo/json/write';
+import Dirs from '@axiona/monorepo/dir/array/dirs.js';
+import UpdateGlobalDependencies from '@axiona/monorepo/update-global-dependencies.js';
+import Files from '@axiona/monorepo/json/array/files.js';
+import JsonRead from '@axiona/monorepo/json-read.js';
+import MergeRight from '@axiona/monorepo/merge-right.js';
+import MergeLeft from '@axiona/monorepo/merge-left.js';
+import Write from '@axiona/monorepo/json/write.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const base = JsonRead(__dirname + '/package.base.json');
 
 Dirs(__dirname, JsonRead(__dirname + '/lerna.json').packages)
--- update-peer-dependency.js
+++ update-peer-dependency.js
@@ -1,10 +1,14 @@
-import UpdatePeerDependencies from '@axiona/monorepo/update-peer-dependencies';
-import Dirs from '@axiona/monorepo/dir/array/dirs';
-import Files from '@axiona/monorepo/file/array/files';
-import Write from '@axiona/monorepo/json/write';
-import JsonRead from '@axiona/monorepo/json-read';
-import PackageJson from '@axiona/monorepo/dir/package-json';
+import UpdatePeerDependencies from '@axiona/monorepo/update-peer-dependencies.js';
+import Dirs from '@axiona/monorepo/dir/array/dirs.js';
+import Files from '@axiona/monorepo/file/array/files.js';
+import Write from '@axiona/monorepo/json/write.js';
+import JsonRead from '@axiona/monorepo/json-read.js';
+import PackageJson from '@axiona/monorepo/dir/package-json.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const dir = PackageJson(__dirname);
 
 Dirs(__dirname, JsonRead(__dirname + '/lerna.json').packages)
